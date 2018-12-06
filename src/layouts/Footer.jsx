2777aad4399fb897ba0b763f1f220c462211229b
danklut <aomcloudenterprise@outlook.com>
2018-12-06 09:02:53 -0800

    Update Footer.jsx

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -25,8 +25,8 @@ const Footer = () => (
   <Wrapper>
     <Text>
       <span>
-        Gatsby Tutorial Starter -{' '}
-        <a href="https://justinformentin.com">Justin Formentin</a>
+        Affiliate Online Marketing -{' '}
+        <a href="https://affiliateonlinemarketing.netlify.com">DanKlut</a>
       </span>
     </Text>
   </Wrapper>
