6f7352e8477532fcba0e106a8ce61e0907df362c
danklut <aomcloudenterprise@outlook.com>
2018-12-06 07:29:14 -0800

    Update blog.jsx

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -10,7 +10,7 @@ const Blog = ({ data }) => {
   return (
     <Layout>
       <Helmet title={'Blog Page'} />
-      <Header title="Blog Page">Gatsby Tutorial Starter</Header>
+      <Header title="Blog Page">Affiliate Online Marketing Blog</Header>
       {edges.map(({ node }) => (
         <BlogList
           key={node.id}
