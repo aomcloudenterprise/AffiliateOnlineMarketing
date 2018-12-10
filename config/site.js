5fa0fcdeca9bcd5d69986aea64d4958dfd8f91fc
danklut <aomcloudenterprise@outlook.com>
2018-12-10 08:34:23 -0800

    Update site.js

--- config/site.js
+++ config/site.js
@@ -6,10 +6,10 @@ module.exports = {
   url: 'https://affiliateonlinemarketing.netlify.com', // Domain of your site. No trailing slash!
   siteUrl: 'https://affiliateonlinemaketing.netlify.com', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
-  logo: 'static/logo/logo.png', // Used for SEO
-  banner: 'static/logo/banner.png',
+  logo: 'static/logo/logo.jpg', // Used for SEO
+  banner: 'static/logo/banner.jpg',
   // JSONLD / Manifest
-  favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
+  favicon: 'static/logo/favicon.jpg', // Used for manifest favicon generation
   shortName: 'AOMCloudBlog', // shortname for manifest. MUST be shorter than 13 characters
   author: 'DanKlut', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
